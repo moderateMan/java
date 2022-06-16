4f6f0a1f10a2003e7e4d24f223c392c090e932af
qanglee <545540710@qq.com>
2022-06-16 14:35:13 +0800

    feat: 实现热部署

--- src/main/java/com/example/demo/controller/HelloWorld.java
+++ src/main/java/com/example/demo/controller/HelloWorld.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloWorld {
+
+    @Value("${test.hello}")
+    private String testHello;
+
+    @Value("${test.hello2:DefaultTest}")
+    private String testHello2;
+
     @GetMapping("/demo")
     public String helloWorld(){
-        return  "helloWorld123";
+        return  "helloWorld"+testHello;
     }
 
     @PostMapping("/demo2")
