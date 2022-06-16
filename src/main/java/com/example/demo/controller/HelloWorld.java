15e39bc6a1910bb900744ada385c726b27cadf8a
qanglee <545540710@qq.com>
2022-06-16 13:37:17 +0800

    feat: 学习使用httpclient并且添加了接口测试

--- src/main/java/com/example/demo/controller/HelloWorld.java
+++ src/main/java/com/example/demo/controller/HelloWorld.java
@@ -1,14 +1,20 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloWorld {
-    @RequestMapping("/demo")
+    @GetMapping("/demo")
     public String helloWorld(){
         return  "helloWorld123";
+    }
+
+    @PostMapping("/demo2")
+    public String demo2(String name){
+        return  "post :"+name;
     }
 }
